2465b6ff525f7a74fcaae4a29e7de148b9b3f529
easa <eisanodehi@gmail.com>
2019-10-16 13:42:54 +0330

    refactor naming is better now

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 const d3 = require('./app/')
 
-var i = d3({ raw: [1, 2, 3, 4, 5] })
-console.log(i('a'))
+var model = d3({ raw: [1, 2, 3, 4, 5] })
 
+var result = model('does not matter yet')
+console.log(result)
 
 module.exports = {
   status: 'uprising'
